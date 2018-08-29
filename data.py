aa12d697f0021421fb21184ae37274611362e55b
humzahkiani89 <40497914+humzahkiani89@users.noreply.github.com>
2018-08-29 14:47:50 -0400

    Add files via upload

--- data.py
+++ data.py
@@ -2,7 +2,8 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
 
-hdulist = fits.open('million.fits')
+
+hdulist = fits.open('45000.fits')
 hdulist.info()
 data = hdulist[1].data
 x=0
@@ -11,14 +12,16 @@ x=0
 #bp = data[:,1]
 mg=[]
 bp=[]
-while (x<1000000):
+while (x<1187):
     z = data[x]
     mg.append(z[0])
     bp.append(z[1])
     x=x+1
 plt.scatter(bp,mg, c='k', s=(20.0/3.0), alpha=0.1, marker=".")
-plt.xlim([0,6])
-plt.ylim([15,-5])
+plt.xlim([0,4])
+plt.ylim([12,-4])
+plt.xticks(np.arange(0, 5, 1.0))
+plt.axes().set_aspect(0.3)
 plt.xlabel('G_BP - G_RP')
 plt.ylabel('M_G')
-plt.show()
+plt.savefig('45000.png')
