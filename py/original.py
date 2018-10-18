732fb7a52398848ae1ea84e4160c23602118df8f
Humzah Kiani <hfk225@nyu.edu>
2018-10-18 04:09:48 -0400

    original graph of high velocity stars

---
+++ py/original.py
@@ -0,0 +1,29 @@
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+hdulist = fits.open('../data/thousand.fits')
+hdulist.info()
+data = hdulist[1].data
+x=0
+#print(data)
+#mg = data[:,0]
+#bp = data[:,1]
+mg=[]
+bp=[]
+while (x<1187):    ##1187
+    z = data[x]
+    mg.append(z[0])
+    bp.append(z[1])
+    x=x+1
+plt.scatter(bp,mg, c='k', s=(0.4), alpha=1, marker=".")
+plt.xlim([0,4])
+plt.ylim([12,-4])
+plt.xticks(np.arange(0, 4, 1.0))
+plt.axes().set_aspect(0.3)
+#plt.axis([0.,4,-4,12])
+plt.xlabel('G_BP - G_RP')
+plt.ylabel('M_G')
+#plt.savefig('45000.png')
+print("hello")
